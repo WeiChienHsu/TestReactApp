98172209c55d75b5a99e2784f25dde9544166f17
WeiChien Hsu <weichien711@gmail.com>
2018-03-14 14:22:09 -0500

    :construction: Create a start file and Test app file (describe/it/expect)

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+import CommentBox from './comment_box';
+import CommentList from './comment_list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div> React simple starter</div>
+    );
+  }
+}
---
+++ test/components/app_test.js
@@ -0,0 +1,15 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+// Use "describe" to group together similar tests
+describe('Test App Component', () => {
+
+    // Use "it" to test a single attribute of a target (Is that show the text)
+    it("Show the correct Text", () => {
+
+        // Use "expect" to make an assertion about a target (I might assume the target is right)
+        expect()
+    });
+
+});
+
